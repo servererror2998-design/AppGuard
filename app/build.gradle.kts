2d913e40af2b31e0247df5a396a9a893e5b857cc
AmoebaKosmik <servererror2998@gmail.com>
2026-09-12 19:00:15 +0700

    Align Java and Kotlin JVM targets and enable Compose compiler

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -14,6 +15,15 @@ android {
         versionCode = 1
         versionName = "1.0.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
